867210f15552d36ed6fdb944276a42b1d176a27c
polydoros97 <polidoros1997@gmail.com>
2019-01-11 14:14:54 +0200

    some minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
-#include <cstring>
 #include "../include/DatabaseSystem.h"
 
 int main(int argc, char const *argv[]) {
     DatabaseSystem* db = new DatabaseSystem();
     db->load_relations();
     db->handle_load();
-    //db->construct_query();
-    //db->execute_query();
     delete db;
     return 0;
 }
